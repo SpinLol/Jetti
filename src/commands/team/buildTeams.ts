a1300310a830384c0a4dfd8281f04bb178176fa7
Phuc Tran <glup3.tran@gmail.com>
2021-01-16 02:15:08 +0100

    add feature: voice + mentions

--- src/commands/team/buildTeams.ts
+++ src/commands/team/buildTeams.ts
@@ -17,68 +17,98 @@ export default class buildTeamsCommand extends Command {
   async run(msg: CommandoMessage, rawArgs: string) {
     const end = new Message(null, null, msg.channel);
 
-    const tries = 10;
-    const neededUsers = 2;
-    const half = Math.ceil(neededUsers / 2);
+    const neededUsers = 4;
+    let userIds: string[];
     const channel = msg.member.voice.channel;
 
     if (rawArgs.trim() === '') {
       if (channel == null) {
-        msg.reply('you are not in a voice channel!');
+        msg.reply('You are not in a voice channel!');
         return end;
       }
 
       if (channel.members.size < neededUsers) {
         msg.say('There are not enough players in the voice channel.');
+        return end;
       } else if (channel.members.size > neededUsers) {
         msg.say('There are too many players in the voice channel. Can not choose players.');
-      } else {
-        const userIds = channel.members.map((_, k) => k);
-        const players = await Player.findAll({ where: { userId: userIds } });
-
-        for (let i = 0; i < tries; i++) {
-          const p = players.slice();
-          shuffle(p);
-
-          const team1 = p.splice(0, half);
-          const team2 = p.splice(-half);
-
-          const skill1 = team1.reduce((a, b) => {
-            return a + b.skillLevel;
-          }, 0);
-          const skill2 = team2.reduce((a, b) => {
-            return a + b.skillLevel;
-          }, 0);
-
-          if (skill1 == skill2 || skill1 + 1 == skill2 || skill1 - 1 == skill2) {
-            const teamNames1 = team1.reduce((a, b) => {
-              const playerName = msg.guild.members.cache.get(b.userId);
-              return ` ${playerName}`;
-            }, '');
-            const teamNames2 = team2.reduce((a, b) => {
-              const playerName = msg.guild.members.cache.get(b.userId);
-              return ` ${playerName}`;
-            }, '');
-            msg.say(`Team 1: ${teamNames1}\nTeam 2: ${teamNames2}`);
-            return end;
-          }
-        }
-
-        msg.say(`Could not create fair teams after trying ${tries} times. Try again`);
+        return end;
       }
 
+      userIds = channel.members.map((_, k) => k);
+      await this.buildTeams(msg, userIds, neededUsers, 10);
       return end;
     }
 
-    // if (msg.mentions.users.size > 0) {
-    //   msg.say(msg.mentions.users.first().username);
-    //   return end;
-    // }
+    if (channel == null && msg.mentions.users.size == 0) {
+      msg.reply('You are not in a voice channel and forgot to mention enough players.');
+      return end;
+    }
 
-    // const args = rawArgs.trim().split(/ +/);
+    if (msg.mentions.users.size == 0) {
+      msg.say('You forgot to choose the remaining players.');
+      return end;
+    }
 
-    // console.log(args);
+    if (channel == null) {
+      msg.reply('You are not in a voice channel.');
+      return end;
+    }
 
+    userIds = channel.members.map((_, k) => k).concat(msg.mentions.users.map((_, k) => k));
+
+    if (userIds.length < neededUsers) {
+      msg.reply(`You didn't mention enough players. ${neededUsers - userIds.length} players are missing.`);
+      return end;
+    } else if (userIds.length > neededUsers) {
+      msg.reply(`You mentioned too many players. You need ${neededUsers} players, not ${userIds.length}`);
+      return end;
+    }
+
+    await this.buildTeams(msg, userIds, neededUsers, 10);
     return end;
+  }
+
+  async buildTeams(msg: CommandoMessage, userIds: string[], playerCount: number, tries: number): Promise<void> {
+    const half = Math.ceil(playerCount / 2);
+    const players = await Player.findAll({ where: { userId: userIds } });
+
+    if (players.length < playerCount) {
+      msg.say(`${playerCount - players.length} players are not in the database. Add them first and try again.`);
+      return;
+    }
+
+    for (let i = 0; i < tries; i++) {
+      const p = players.slice();
+      shuffle(p);
+
+      const team1 = p.splice(0, half);
+      const team2 = p.splice(-half);
+
+      console.log(team1, team2);
+
+      const skill1 = team1.reduce((a, b) => {
+        return a + b.skillLevel;
+      }, 0);
+      const skill2 = team2.reduce((a, b) => {
+        return a + b.skillLevel;
+      }, 0);
+
+      if (skill1 == skill2 || skill1 + 1 == skill2 || skill1 - 1 == skill2) {
+        const teamNames1 = team1.reduce((a, b) => {
+          const playerName = msg.guild.members.cache.get(b.userId);
+          return `${a} ${playerName}`;
+        }, '');
+        const teamNames2 = team2.reduce((a, b) => {
+          const playerName = msg.guild.members.cache.get(b.userId);
+          return `${a} ${playerName}`;
+        }, '');
+
+        msg.say(`Team 1: ${teamNames1}\n\nTeam 2: ${teamNames2}`);
+        return;
+      }
+    }
+
+    msg.say(`Could not create fair teams after trying ${tries} times. Try again.`);
   }
 }

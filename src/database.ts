c5ac0fd6ebbc772406b2bc0dabdd4079878a9bbb
Phuc Tran <glup3.tran@gmail.com>
2021-01-16 21:50:51 +0100

    try manual add model

--- src/database.ts
+++ src/database.ts
@@ -1,4 +1,5 @@
 import { Sequelize } from 'sequelize-typescript';
+import Player from './db/models/Player.model';
 
 const sequelize =
   process.env.NODE_ENV == 'development'
@@ -8,6 +9,6 @@ const sequelize =
       })
     : new Sequelize(process.env.DATABASE_URL);
 
-sequelize.addModels([__dirname + '/**/*.model.ts']);
+sequelize.addModels([Player]);
 
 export { sequelize };

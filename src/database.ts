7d2797c84bcf33e78c324485df5e5da90a2e53b8
Phuc Tran <glup3.tran@gmail.com>
2021-01-16 03:57:59 +0100

    use postgres in production

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,12 @@
 import { Sequelize } from 'sequelize-typescript';
 
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: 'src/db/db.sqlite',
-  models: [__dirname + '/**/*.model.ts'],
-});
+const sequelize =
+  process.env.NODE_ENV == 'development'
+    ? new Sequelize({
+        dialect: 'sqlite',
+        storage: 'src/db/db.sqlite',
+        models: [__dirname + '/**/*.model.ts'],
+      })
+    : new Sequelize(process.env.DATABASE_URL);
 
 export { sequelize };

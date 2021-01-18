72fcc6465b63d077b6faab2420c481624fa08b3e
Jakob R <Sicari-s@users.noreply.github.com>
2021-01-19 00:29:42 +0100

    Refined store match image to include match result

--- src/commands/match/storeMatchImage.ts
+++ src/commands/match/storeMatchImage.ts
@@ -5,28 +5,34 @@ import { Match } from '../../db/models';
 
 interface PromptArgs {
   matchId: number;
+  matchResult: number;
 }
 
 export default class StoreImageCommand extends Command {
   constructor(client: CommandoClient) {
     super(client, {
       name: 'store-image-match',
-      aliases: ['sim'],
+      aliases: ['sim', 'screenshot'],
       group: 'match',
       memberName: 'store-image-match',
       description: 'Stores the link of the game screenshot as a link (string). Image must be embedded with message.',
-      argsCount: 1,
+      argsCount: 2,
       args: [
         {
           key: 'matchId',
           prompt: 'What is the match ID?',
           type: 'integer',
         },
+        {
+          key: 'matchResult',
+          prompt: 'Who won? T1 => 1, T2 => 2, Draw => 0',
+          type: 'integer',
+        },
       ],
     });
   }
 
-  async run(msg: CommandoMessage, { matchId }: PromptArgs) {
+  async run(msg: CommandoMessage, { matchId, matchResult }: PromptArgs) {
     const end = new Message(null, null, msg.channel);
 
     if (matchId == undefined) {
@@ -41,15 +47,22 @@ export default class StoreImageCommand extends Command {
       return end;
     }
 
+    if (matchResult < 0 || matchResult > 2) {
+      msg.say(`Match result \`${matchId}\` is invalid! Must be 0, 1 or 2`);
+      return end;
+    }
+
     if (msg.attachments.size == 0) {
       msg.say(`No attached image detected. Write command as comment when uploading image.`);
       return end;
     }
 
+    match.matchResult = matchResult;
     match.screenshotPath = msg.attachments.first().url;
     match.save();
 
-    msg.say('Added `' + match.screenshotPath + `\` to Match with ID \`${match.id}\``); //.url is same as.attachment
+
+    msg.say(`Added \`${match.screenshotPath}\` to Match with ID \`${match.id}\`, and set result as\`${match.matchResult}\`); //.url is same as.attachment
     return end;
   }
 }

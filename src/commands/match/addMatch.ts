33eed840b9e22148134f75ff46586ded5312f7ba
Phuc Tran <glup3.tran@gmail.com>
2021-01-18 15:44:53 +0100

    add commands 'add' & 'get' match

---
+++ src/commands/match/addMatch.ts
@@ -0,0 +1,62 @@
+import { Message } from 'discord.js';
+import { Command, CommandoClient, CommandoMessage } from 'discord.js-commando';
+
+import { Match, Team } from '../../db/models';
+
+interface PromptArgs {
+  teamId1: number;
+  teamId2: number;
+}
+
+export default class AddMatchCommand extends Command {
+  constructor(client: CommandoClient) {
+    super(client, {
+      name: 'add-match',
+      aliases: ['am'],
+      group: 'team',
+      memberName: 'add-match',
+      description: 'Creates a new match with 2 teams',
+      argsCount: 2,
+      args: [
+        {
+          key: 'teamId1',
+          prompt: 'What is the ID from team1?',
+          type: 'integer',
+        },
+        {
+          key: 'teamId2',
+          prompt: 'What is the ID from team1?',
+          type: 'integer',
+        },
+      ],
+    });
+  }
+
+  async run(msg: CommandoMessage, { teamId1, teamId2 }: PromptArgs) {
+    const end = new Message(null, null, msg.channel);
+
+    const team1 = await Team.findOne({ where: { id: teamId1 } });
+
+    if (team1 == null) {
+      msg.say(`Couldn't find team 1 with ID ${teamId1}`);
+      return end;
+    }
+
+    const team2 = await Team.findOne({ where: { id: teamId2 } });
+
+    if (team2 == null) {
+      msg.say(`Couldn't find team 2 with ID ${teamId2}`);
+      return end;
+    }
+
+    const match = new Match({
+      teamId1: team1.id,
+      teamId2: team2.id,
+    });
+
+    await match.save();
+
+    msg.say(`Successfully created Match (ID: ${match.id}) with Team1 (ID: ${team1.id}) and Team2 (ID: ${team2.id})`);
+    return end;
+  }
+}
--- src/db/models/Match.model.ts
+++ src/db/models/Match.model.ts
@@ -1,9 +1,9 @@
-import { Column, Model, Table, ForeignKey, BelongsTo, DataType } from 'sequelize-typescript';
+import { Column, Model, Table, ForeignKey, BelongsTo } from 'sequelize-typescript';
 import Team from '../../db/models/Team.model';
 
 @Table
 export default class Match extends Model {
-  @Column
+  @Column({ defaultValue: 0 })
   matchResult: number; //0 = draw, 1 = t1 won, 2 = t2 won
 
   @Column
@@ -22,4 +22,14 @@ export default class Match extends Model {
 
   @BelongsTo(() => Team, 'teamId2')
   team2: Team;
+
+  getOutcome(): string {
+    if (this.matchResult == 1) {
+      return 'Team 1 won';
+    } else if (this.matchResult == 2) {
+      return 'Team 2 won';
+    }
+
+    return 'Draw';
+  }
 }

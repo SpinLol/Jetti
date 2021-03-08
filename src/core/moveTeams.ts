6c9f0f41781eabf0d932a4a4b6aeefe8a07665e3
Phuc Tran <glup3.tran@gmail.com>
2021-03-08 19:19:40 +0100

    fix not found user

--- src/core/moveTeams.ts
+++ src/core/moveTeams.ts
@@ -7,7 +7,13 @@ export const moveTeams = async (team: Team, channel: GuildChannel, msg: Commando
 
   for (const player of players) {
     const user = await msg.guild.members.fetch(player.player.userId);
-    if (user.voice.channel != null) {
+
+    if (user == null) {
+      await msg.say(`Couldn't find user \`${player.player.userTag}\``);
+      continue;
+    }
+
+    if (user?.voice.channel != null) {
       await user.voice.setChannel(channel);
     } else {
       await msg.say(`Didn't move \`${user.displayName}\` because he/she isn't in a voice channel`);

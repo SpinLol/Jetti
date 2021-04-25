7d5dbd7e8e4479d13ba0a81ef2a4c30a57a8d582
Easyzsz <iskren.i@gmx.at>
2021-04-25 23:18:57 +0200

    refactor randomMap command

--- src/commands/match/randomMap.ts
+++ src/commands/match/randomMap.ts
@@ -6,8 +6,8 @@ export const allMaps = ['ASCENT', 'BIND', 'HAVEN', 'ICEBOX', 'SPLIT'] as const;
 export type ValorantMaps = typeof allMaps;
 
 interface PromptArgs {
-  biased: 'r' | 'b';
-  choice: 'y' | 'n';
+  isBiased: boolean;
+  areTwoMaps: boolean;
 }
 
 export default class RandomMapCommand extends Command {
@@ -21,102 +21,68 @@ export default class RandomMapCommand extends Command {
       argsCount: 2,
       args: [
         {
-          key: 'biased',
-          prompt: 'Want to choose random or biased? "r/b"',
-          type: 'string',
-          oneOf: ['r', 'b'],
+          key: 'isBiased',
+          prompt: 'Should the map be chosen biased? "y/n"',
+          type: 'boolean',
         },
         {
-          key: 'choice',
+          key: 'areTwoMaps',
           prompt: 'Want to choose from two maps? "y/n"',
-          type: 'string',
-          oneOf: ['y', 'n'],
+          type: 'boolean',
         },
       ],
     });
   }
 
-  async run(msg: CommandoMessage, { biased, choice }: PromptArgs) {
+  async run(msg: CommandoMessage, { isBiased, areTwoMaps }: PromptArgs) {
     const channel = msg.member.voice.channel;
+
     if (channel == null) {
       return msg.reply('You are not in a voice channel!');
     }
+
     const userIds = channel.members.map((_, k) => k);
-
     const players = await Player.findAll({ where: { userId: userIds } });
+    const poolMaps: string[] = [];
 
-    if (biased == 'r') {
-      const c = Math.floor(Math.random() * allMaps.length);
-
-      if (choice == 'y') {
-        return msg.say(allMaps[c]);
-      } else {
-        // eslint-disable-next-line no-constant-condition
-        while (true) {
-          const z = Math.floor(Math.random() * allMaps.length);
-          if (z != c) {
-            const pollTopic = await msg.say(['✌️ :' + allMaps[c], '😙 :' + allMaps[z]]);
-
-            await pollTopic.react('✌️');
-            await pollTopic.react('😙');
-            const filter = (reaction: MessageReaction) => reaction.emoji.name === '✌️' || reaction.emoji.name === '😙';
-
-            const major = Math.floor(players.length / 2) + 1;
-            const collector = pollTopic.createReactionCollector(filter, { time: 15000 });
-            collector.on('collect', (r) => {
-              if (r.count > major) {
-                if (r.emoji.toString() === '✌️') {
-                  collector.stop();
-                  return msg.say(allMaps[c] + ' has won!');
-                }
-                if (r.emoji.toString() === '😙') {
-                  collector.stop();
-                  return msg.say(allMaps[z] + ' has won!');
-                }
-              }
-            });
-
-            break;
-          }
-        }
-      }
+    if (isBiased == false) {
+      poolMaps.push(...allMaps);
+    } else {
+      poolMaps.push(...players.map((p) => p.favMap));
     }
-    if (biased == 'b') {
-      const biasMaps = players.map((p) => p.favMap);
 
-      const c = Math.floor(Math.random() * biasMaps.length);
-      if (choice == 'y' || biasMaps.length == 1) {
-        return msg.say(biasMaps[c]);
-      } else {
-        // eslint-disable-next-line no-constant-condition
-        while (true) {
-          const z = Math.floor(Math.random() * biasMaps.length);
-          if (z != c) {
-            const pollTopic = await msg.say(['✌️ :' + biasMaps[c], '😙 :' + biasMaps[z]]);
+    const index1 = Math.floor(Math.random() * poolMaps.length);
 
-            await pollTopic.react('✌️');
-            await pollTopic.react('😙');
-            const filter = (reaction: MessageReaction) => reaction.emoji.name === '✌️' || reaction.emoji.name === '😙';
-
-            const major = Math.floor(players.length / 2) + 1;
-            const collector = pollTopic.createReactionCollector(filter, { time: 15000 });
-            collector.on('collect', (r) => {
-              if (r.count > major) {
-                if (r.emoji.toString() === '✌️') {
-                  collector.stop();
-                  return msg.say(biasMaps[c] + ' has won!');
-                }
-                if (r.emoji.toString() === '😙') {
-                  collector.stop();
-                  return msg.say(biasMaps[z] + ' has won!');
-                }
-              }
-            });
-
-            break;
-          }
-        }
-      }
+    if (areTwoMaps == false || [...new Set(poolMaps)].length == 1) {
+      return msg.say(poolMaps[index1]);
     }
+
+    let index2 = 0;
+
+    do {
+      index2 = Math.floor(Math.random() * poolMaps.length);
+    } while (poolMaps[index2] == poolMaps[index1]);
+
+    const pollTopic = await msg.say([`✌️ : ${poolMaps[index1]}`, `😙 : ${poolMaps[index2]}`]);
+
+    await pollTopic.react('✌️');
+    await pollTopic.react('😙');
+
+    const major = Math.floor(players.length / 2) + 1;
+    const filter = (reaction: MessageReaction) => reaction.emoji.name === '✌️' || reaction.emoji.name === '😙';
+    const collector = pollTopic.createReactionCollector(filter, { time: 1000 * 10 });
+
+    collector.on('collect', (r) => {
+      if (r.count > major) {
+        collector.stop();
+        msg.say(poolMaps[r.emoji.toString() === '✌️' ? index1 : index2] + ' has won!');
+      }
+    });
+    collector.on('end', async (_, reason) => {
+      if (reason == 'time') {
+        await msg.say('Voting over, time has ended.');
+        await msg.say("Guess we're playing ICEBOX!");
+      }
+    });
   }
 }

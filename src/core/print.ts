a1584a14e767e1a919ff1456c293503c34025bfe
Phuc Tran <glup3.tran@gmail.com>
2021-04-25 20:39:31 +0200

    comment out not working commands

--- src/core/print.ts
+++ src/core/print.ts
@@ -1,24 +1,4 @@
 import { GetPlayerQuery, GetTeamQuery } from '../api/generated/graphql';
-import { Team } from '../db/models';
-
-export function printTeam(team: Team): string {
-  const skillTotal =
-    team.player1.skillLevel +
-    team.player2.skillLevel +
-    team.player3.skillLevel +
-    team.player4.skillLevel +
-    team.player5.skillLevel;
-
-  let res = '```\n';
-  res += `Team ${team.teamName} (Total Skill: ${skillTotal} | ID: ${team.id})`;
-  res += `\n\tLevel ${team.player1.skillLevel}\t${team.player1.userTag}`;
-  res += `\n\tLevel ${team.player2.skillLevel}\t${team.player2.userTag}`;
-  res += `\n\tLevel ${team.player3.skillLevel}\t${team.player3.userTag}`;
-  res += `\n\tLevel ${team.player4.skillLevel}\t${team.player4.userTag}`;
-  res += `\n\tLevel ${team.player5.skillLevel}\t${team.player5.userTag}`;
-  res += '\n```';
-  return res;
-}
 
 export function playerToString(player: GetPlayerQuery['player'] | GetTeamQuery['team']['PlayerH1']): string {
   return `${player.userTag} (${printLevelName(player.skillLevel)})`;
